e84f98d9c166378cc4c3045a39b9610573a8631d
Franco Selvarolo <fselvarolo28@gmail.com>
2022-11-02 17:45:47 -0300

    720p

--- src/components/carrusel/Carusel.jsx
+++ src/components/carrusel/Carusel.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import BBimg from "../assets/blockbusterImg.jpg"
+import BBimg from "../assets/bbPic.png"
 import GamesImg from "../assets/GameU.jpg"
-import CountryImg from "../assets/countries img.jpg"
+import CountryImg from "../assets/Countries.png"
 import SvalyImg from "../assets/svaly1.jpg"
 import "./carusel.css"
 
@@ -18,45 +18,57 @@ export const Carousel = ()=>{
   <div className="carousel-inner">
     <a href="https://blockbuster-pf.vercel.app/" target="_blank" rel="noreferrer" >
     <div className="carousel-item active">
-    
-      <p className="project">Block Buster</p>
-      <p className="projectDate">10 / 10/ 22</p>
+      <img src={BBimg} className="d-block" alt="..."/>
+      <div className="backInfo1">
+        <p className="projectDate">10 / 10/ 22</p>
+      <p className="project"> Block Buster</p>
+     
       <p className="projectDet">Led the creation of an OTT streaming platform/service for online movies and series.</p>
       <p className="projectDet1"><b>Role: </b> Desing & Development.</p>
       <p className="projectDet2"><b>Made With: </b>  TypeScript, Sequelize, Express and PostgreSQL & Nodemailer notifications in the Back-End and React, Redux-Toolkit, Cloudinary, auth0(google log-in) & Paypal payment in Front-End.</p>
- 
+
+      </div>
       
-      <img src={BBimg} className="d-block" alt="..."/>
-    </div>  </a>
+    </div> 
+    </a>
     <div className="carousel-item">
-    <p className="project">VideoGames</p>
-      <p className="projectDate">25 / 7 / 22</p>
+      <img src={GamesImg} className="d-block " alt="..."/>
+      <div className="backInfo2">
+
+        <p className="projectDate">25 / 7 / 22</p>
+         <p className="project">  VideoGames</p>
+
+   
       <p className="projectDet">included: searches, filtering, ordering and creation or addition of information</p>
       
       <p className="projectDet1"><b>Role: </b> Desing & Development.</p>
       <p className="projectDet2"><b>Made With: </b>JavaScript, React, Redux, CSS, HTML5, Express, Sequalize, Node, SQL </p>
  
-      <img src={GamesImg} className="d-block " alt="..."/>
+      </div>
     </div>
     <div className="carousel-item">
-    <p className="project">Countries</p>
+      <img src={CountryImg} className="d-block" alt="..."/>
+      <div className="backInfo3">
       <p className="projectDate">25 / 8 / 22</p>
+    <p className="project">Countries</p>
       <p className="projectDet">includes: searches, filtering, ordering and creation or addition of information</p>
     
       <p className="projectDet1"><b>Role: </b> Desing & Development.</p>
       <p className="projectDet2"><b>Made With: </b>JavaScript, React, Redux, CSS, HTML5, Express, Sequalize, Node, SQL </p>
- 
+      </div>
       
-      <img src={CountryImg} className="d-block" alt="..."/>
     </div>
     <div className="carousel-item">
-    <p className="project">Svaly - Shifter</p>
+      <img src={SvalyImg} className="d-block" alt="..."/>
+      <div className="backInfo4">
       <p className="projectDate">25 / 10 / 20</p>
+        
+    <p className="project">Svaly - Shifter</p>
       <p className="projectDet">application to make daily reservations in gyms, with personal acount and stats</p>
       
       <p className="projectDet1"><b>Role: </b> Desing & Development.</p>
       <p className="projectDet2"><b>Made With: </b>JavaScript, React, Redux, CSS, HTML5, Express, Sequalize, Node, SQL </p>
-      <img src={SvalyImg} className="d-block" alt="..."/>
+      </div>
     </div>
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
