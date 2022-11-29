2941c866bfe831f92a81917d7c4a9044c7cc3847
Franco Selvarolo <fselvarolo28@gmail.com>
2022-11-29 08:34:52 -0300

    no totop

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import { Nav } from "./components/Nav/nav";
 import { AbourMe } from "./components/AboutMe/aboutMe";
 import { Certificates } from "./components/Certifcado/credentials";
 import { Project } from "./components/Proj/projects";
-import { ToTop } from "./components/Footer/backToTop";
+
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <ToTop />
+ 
       <Header />
       <AbourMe />
       <Project />
